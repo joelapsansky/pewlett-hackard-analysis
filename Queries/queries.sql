34561ad2d80d7b009d3caf71559546aabf1e8073
Joe Lapsansky <joe.lapsansky@gmail.com>
2021-01-23 14:13:52 -0600

    adding created SQL queries and export file

---
+++ Queries/queries.sql
@@ -0,0 +1,24 @@
+-- Query dept_emp
+SELECT * FROM dept_emp;
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Use select into before exporting needed data
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Query new retirement_info table
+SELECT * FROM retirement_info;
