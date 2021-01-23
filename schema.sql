79a9adaccc3aaa140ffc653ba4c700cda3422871
Joe Lapsansky <joe.lapsansky@gmail.com>
2021-01-23 13:52:45 -0600

    updating sql file

--- schema.sql
+++ schema.sql
@@ -44,12 +44,12 @@ CREATE TABLE titles (
   	PRIMARY KEY (emp_no, title, from_date)
 );
 
-CREATE TABLE dept_employee (
-  	dept_no VARCHAR(4) NOT NULL,
- 	emp_no INT NOT NULL,
+CREATE TABLE dept_emp (
+  	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
   	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
-  	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (dept_no, emp_no)
+  	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  	PRIMARY KEY (emp_no, dept_no)
 );
